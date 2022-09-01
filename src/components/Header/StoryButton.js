0827e12b5d9a8ab32bf4c69ce1777631a7f45358
Adebanjo Afolasope <folasopeadebanjo@gmail.com>
2022-09-01 20:39:19 +0100

    modified add story button

--- src/components/Header/StoryButton.js
+++ src/components/Header/StoryButton.js
@@ -1,8 +1,13 @@
+import styles from './StoryButton.module.css';
 
 const StoryButton = (props) => {
-    return (
-      <button>Add your story</button>
-    );
+  return (
+    <button className={styles.btn}>
+      <span >+</span>
+      <span className={styles}>Add your</span>
+      <span>story</span>
+    </button>
+  );
 };
 
 export default StoryButton;
