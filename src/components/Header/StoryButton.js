9a283bfb8ac26804f4aa6b51ed3f51cd41de8398
Adebanjo Afolasope <folasopeadebanjo@gmail.com>
2022-09-01 21:09:28 +0100

    adds responsiveness to header

--- src/components/Header/StoryButton.js
+++ src/components/Header/StoryButton.js
@@ -1,9 +1,10 @@
 import styles from './StoryButton.module.css';
+import {BsPlusLg} from 'react-icons/bs'
 
 const StoryButton = (props) => {
   return (
     <button className={styles.btn}>
-      <span >+</span>
+      <span ><BsPlusLg/></span>
       <span className={styles}>Add your</span>
       <span>story</span>
     </button>
